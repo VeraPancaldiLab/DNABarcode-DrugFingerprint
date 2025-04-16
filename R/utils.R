481baf555f50c3aed0c4b6b8d79e5852f68a0510
bastienchassagnol <bastien_chassagnol@laposte.net>
2025-04-15 21:25:35 -0400

    homogenise metadata information, integrating latest time point experiences

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,9 @@
 compare_dataframes <- function(df1, df2) {
 
   # compare dimensions and ids
-  #testthat::expect_equal(dim(df1), dim(df2), info = "Column counts differ")
-  message(all.equal(df1, df2), "\n\n")
+  # message(all.equal(df1, df2), "\n\n")
 
-  unique_df1_ids <- setdiff(df1$key_barcode, df2$key_barcode)
+  unique_df1_ids <- setdiff(df1$barcode_id, df2$barcode_id)
   if (length(unique_df1_ids)==0) {
     message("All barcode IDs in df1 are avalaible in df2.")
   } else{
@@ -12,7 +11,7 @@ compare_dataframes <- function(df1, df2) {
                   " barcode IDs in df1 are missing in df2."))
   }
 
-  unique_df2_ids <- setdiff(df2$key_barcode, df1$key_barcode)
+  unique_df2_ids <- setdiff(df2$barcode_id, df1$barcode_id)
   if (length(unique_df2_ids)==0) {
     message("All barcode IDs in df2 are avalaible in df1.")
   } else{
@@ -21,17 +20,18 @@ compare_dataframes <- function(df1, df2) {
   }
 
   # compare shared content (same colnames and barcode IDs)
-  shared_barcodes <- tibble::tibble(key_barcode =
-                                      intersect(df2$key_barcode, df1$key_barcode))
+  shared_barcodes <- tibble::tibble(barcode_id =
+                                      intersect(df2$barcode_id, df1$barcode_id))
   shared_colnames <- intersect(colnames(df1), colnames(df2))
   df1_consensual <- df1 |>
-    semi_join(df2, by = "key_barcode") |>
+    semi_join(df2, by = "barcode_id") |>
     select(all_of(shared_colnames)) |>
-    arrange(key_barcode)
+    arrange()
+
   df2_consensual <- df2 |>
-    semi_join(df1, by = "key_barcode") |>
-    select(all_of(shared_colnames)) |>
-    arrange(key_barcode)
+    semi_join(df1, by = "barcode_id") |>
+    dplyr::select(all_of(shared_colnames)) |>
+    dplyr::arrange(barcode_id)
 
   testthat::expect_equal(df1_consensual, df2_consensual,
                          info = "On the shared dimensions, dataframes are not equivalent")
@@ -47,7 +47,7 @@ compare_dataframes <- function(df1, df2) {
 }
 
 
-format_number <- function(x) {
+format_concentrations <- function(x) {
   x_formatted <- sapply(x, function(number_to_format) {
     if (number_to_format %% 1 == 0) {  # Check if x is an integer
       sprintf("%03d", number_to_format)  # Left-pad integers to 3 digits
--- run_analyses.R
+++ run_analyses.R
@@ -1,17 +1,32 @@
 library(dplyr)
 
-expression_files <- list.files("./data/barcode-counts/",
-                               pattern = "exp.*\\.xlsx",
-                               full.names = TRUE)
+# expression_files <- list.files("./data/barcode-counts/",
+#                                pattern = "exp.*\\.xlsx",
+#                                full.names = TRUE)
+#
+# # change format + barcode name
+# expression_matrices <- lapply(expression_files, function(old_filename) {
+#   expression_matrix <- readxl::read_excel(old_filename) |>
+#     dplyr::rename(barcode_id = 1)
+#
+#   new_filename <- old_filename |> basename() |> tools::file_path_sans_ext()
+#
+#   readr::write_csv(expression_matrix,
+#                    file = file.path("data", "barcode-counts", paste0(new_filename, ".csv")))
+#
+# })
+
+barcode_files <- list.files("./data/barcode-counts/",
+                            pattern = "exp.*\\.csv",
+                            full.names = TRUE)
 
 # change format + barcode name
-expression_matrices <- lapply(expression_files, function(old_filename) {
-  expression_matrix <- readxl::read_excel(old_filename) |>
-    dplyr::rename(barcode_id = 1)
-
-  new_filename <- old_filename |> basename() |> tools::file_path_sans_ext()
-
-  readr::write_csv(expression_matrix,
-                   file = file.path("data", "barcode-counts", paste0(new_filename, ".csv")))
-
-})
+barcode_summaries <- purrr::map(barcode_files, function(filename) {
+  barcode_counts <- readr::read_csv(filename, show_col_types = FALSE)
+  experience_name <- filename |> basename() |> tools::file_path_sans_ext()
+  experience_summary <- tibble::tibble(`Experience Name`= experience_name,
+                                       Features= paste0("Barcode matrix contains: ", nrow(barcode_counts),
+                                                        " unique barcode IDs, and ", ncol(barcode_counts)-1, " replicates."))
+  return(experience_summary)
+}) |>
+  purrr::list_rbind()
